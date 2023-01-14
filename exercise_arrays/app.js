6c5644e9eeedf4e6544d0689a6180e8d94d78115
salcasta <salcasta@gmail.com>
2023-01-13 21:33:55 -0600

    first couple exercises with arrays

---
+++ exercise_arrays/app.js
@@ -0,0 +1,16 @@
+// Lotto Numbers Exercise
+// Let's get some practice creating arrays. Please define a variable called lottoNumbers and set it equal to an array containing any 6 numbers.  It's simple, but we've got to start somewhere!
+
+const lottoNumbers = [5, 15, 47, 62, 89, 96];
+
+// Array Access Exercise
+// I've provided you with a leaderboard array, which consists of 4 strings.  Without touching line 1, please make the following changes:
+
+// Unfortunately, I misspelled the 2nd name in the leaderboard array.I meant to write "Luna" instead of "Lua".Please update it to "Luna"(without changing line 1)
+
+// Bellatrix has fallen off the leaderboard and has been replaced by Draco! Please update the last element of the array so that it holds "Draco" instead of "Bellatrix"(once again, please do this without changing line 1)
+
+const leaderboard = ['Harry', 'Lua', 'Hermione', 'Bellatrix'];
+
+leaderboard[1] = 'Luna';
+leaderboard[3] = 'Draco';
