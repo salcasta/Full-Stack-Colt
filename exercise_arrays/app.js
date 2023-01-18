377713ad4808513d09741ae64f189b98e20f0c31
salcasta <salcasta@gmail.com>
2023-01-17 20:37:43 -0600

    finish array section

--- exercise_arrays/app.js
+++ exercise_arrays/app.js
@@ -14,3 +14,36 @@ const leaderboard = ['Harry', 'Lua', 'Hermione', 'Bellatrix'];
 
 leaderboard[1] = 'Luna';
 leaderboard[3] = 'Draco';
+
+
+// Push / Pop / Shift / Unshift Practice
+// I've provided you with an array called planets. Unfortunately, I'm an idiot who doesn't know much about our solar system and I made some mistakes.  Please help me fix the planets array!  Using the array methods we just learned...
+
+// Remove the first element, "The Moon", from the planets array.The moon is not a planet!
+
+// Add in "Saturn" at the very end of the planets array
+
+// Add "Mercury" as the first element in the planets array.
+
+// At the end of your hard work, planets should look like this: ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn"]
+
+const planets = ['The Moon', 'Venus', 'Earth', 'Mars', 'Jupiter'];
+
+planets.shift();
+planets.push('Saturn');
+planets.unshift('Mercury');
+
+
+
+// Nested Arrays Exercise
+// I've provided you with an array called airplaneSeats, which represents the rows of seats on a flight.  Our friend Hugo is looking to book a seat on our flight. Fortunately, there is one middle seat available (it's set to null currently).  Please update the array element that is currently null, to instead be "Hugo".
+
+const airplaneSeats = [
+    ['Ruth', 'Anthony', 'Stevie'],
+    ['Amelia', 'Pedro', 'Maya'],
+    ['Xavier', 'Ananya', 'Luis'],
+    ['Luke', null, 'Deniz'],
+    ['Rin', 'Sakura', 'Francisco']
+];
+
+airplaneSeats[3][1] = 'Hugo';
